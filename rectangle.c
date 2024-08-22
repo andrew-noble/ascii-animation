c0487a1447402f03f0a44fa267c68dd8a81d43bc
Andrew Noble <aknoble.andrew@gmail.com>
2024-08-21 18:37:20 -0800

    Rectangle raster

--- rectangle.c
+++ rectangle.c
@@ -3,6 +3,10 @@
 #include <string.h>
 
 //asuming a raster-like behavior for printing, 
+
+//ask user for length, width (maybe also incorporate into the cmd line args)
+
+
 //loop len times:
     //loop len times:
         //print #
@@ -13,27 +17,31 @@
 
 int main() {
 
-    int len;
+    int width;
+    int height;
     int i; //iterator outer
     int j; //outer iterator
 
-    printf("How long do you want the sides of the square to be?\n");
-    scanf("%d", &len); //to scanf, I pass a format string to indicate we're receiving a digit, and a reference of len, which scanf will use to load w/ user input
+    printf("How wide?\n");
+    scanf("%d", &width); 
+
+    printf("How tall?\n");
+    scanf("%d", &height); 
 
     // Hide cursor
-    printf("\e[?25l"); 
+    // printf("\e[?25l"); 
 
-    for (i = 0; i < len; i++) {
-        for (j = 0; j < len; j++) {
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
             putchar('#'); //just prints character (but also moves cursor ahead)
             fflush(stdout); //this forces printing to the console (stdout is buffered)
-            // usleep(100000);
+            usleep(100000);
         }
         printf("\n");
     }
 
     usleep(100000);
-    printf("\e[%dE\n", len); //move the cursor out of the way, depending on how big the box was
+    printf("\e[%dE", height); //move the cursor out of the way, depending on how big the box was
     printf("\e[?25h\n");    // Show cursor
 
 
